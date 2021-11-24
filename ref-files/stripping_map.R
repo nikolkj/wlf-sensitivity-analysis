0963c9fa1fc1155b0710ac9b2a229949e6d77485
nikolkj <nikolaipriser@gmail.com>
2021-11-24 04:42:15 -0600

    initial commit; reference files.

---
+++ ref-files/stripping_map.R
@@ -0,0 +1,17 @@
+# reference-file list object
+# ... maps characters to symbolic equivalents for replacement
+# ... modify as needed.
+strip_map =  list(
+  a = c("@"),
+  b = c("6"),
+  s = c("$", "5"),
+  S = c("$", "5"),
+  B = c("8"),
+  z = c("2"),
+  Z = c("2"),
+  I = c("1", "!", "|"),
+  l = c("1", "!", "|"),
+  L = c("1", "!", "|"),
+  o = c("()", "0"),
+  O = c("()", "0")
+)  
